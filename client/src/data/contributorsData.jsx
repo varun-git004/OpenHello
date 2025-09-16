99b2208b8cac2572fbfa85550a7f65ffbb17d70b
TaneeshCodes <taneesh.sawant0110@gmail.com>
2025-09-16 11:26:46 +0530

    Update contributorsData.jsx

--- client/src/data/contributorsData.jsx
+++ client/src/data/contributorsData.jsx
@@ -4,6 +4,11 @@ export const contributorsData = [
     photoUrl: "https://avatars.githubusercontent.com/u/111569738?v=4",
     githubUrl: "https://github.com/sidxhdev",
   },
+  {
+    name: "Taneesh Sawant",
+    photoUrl: "https://avatars.githubusercontent.com/u/179444959?v=4",
+    githubUrl: "https://github.com/TaneeshCodes",
+  },
   
  
   // Add more contributors here
