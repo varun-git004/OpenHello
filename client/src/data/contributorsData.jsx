6665742acaa827e74e15aa8eb017b2f5a46e1324
geetika kadam <kadamgeetika@gmail.com>
2025-09-17 13:17:57 +0530

    Update contributorsData.jsx

--- client/src/data/contributorsData.jsx
+++ client/src/data/contributorsData.jsx
@@ -4,6 +4,12 @@ export const contributorsData = [
     photoUrl: "https://avatars.githubusercontent.com/u/111569738?v=4",
     githubUrl: "https://github.com/sidxhdev",
   },
+  {
+    name: "Geetika kadam",
+    photourl: "https://avatars.githubusercontent.com/u/232422710?v=4",
+    gituburl:"https://github.com/kadamgeetika-cpu",
+  },
+    
   
  
   // Add more contributors here
