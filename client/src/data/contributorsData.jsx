df47d2ba5cb1f97242579ef9168c8017d9bf217b
Siddesh Navthale <navthalesiddesh@gmail.com>
2025-09-17 15:12:57 +0530

    Update Deep Keni's photo URL in contributorsData

--- client/src/data/contributorsData.jsx
+++ client/src/data/contributorsData.jsx
@@ -6,7 +6,7 @@ export const contributorsData = [
   },
   {
     name: "Deep Keni",
-    photoUrl: "https://avatars.githubusercontent.com/u/220671308?s=400&u=1a50b953088645caf2cd88e42eca8d0b5627d1d9&v=4",
+    photoUrl: "https://avatars.githubusercontent.com/u/111569738?v=4",
     githubUrl: "https://github.com/Deep-keni/Deep-Keni",
   },
     {
