5046de4f29b57fe08a259f751be5bb9370207cc0
anildyadav2004-gif <anildyadav2004@gmail.com>
2025-09-17 13:14:06 +0530

    Update contributorsData.jsx

--- client/src/data/contributorsData.jsx
+++ client/src/data/contributorsData.jsx
@@ -4,6 +4,12 @@ export const contributorsData = [
     photoUrl: "https://avatars.githubusercontent.com/u/111569738?v=4",
     githubUrl: "https://github.com/sidxhdev",
   },
+
+  {
+    name: "Anil Yadav",
+    photoUrl: "https://avatars.githubusercontent.com/u/155896766?v=4",
+    githubUrl: "https://github.com/Anilyadav20",
+  },
   
  
   // Add more contributors here
