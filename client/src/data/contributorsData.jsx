b4603fab7be6329f48717ed13d34b1878036ff05
KAVYA SHAH <kavyashah-cmpn@atharvacoe.ac.in>
2025-09-17 13:09:23 +0530

    Update contributorsData.jsx

--- client/src/data/contributorsData.jsx
+++ client/src/data/contributorsData.jsx
@@ -4,6 +4,11 @@ export const contributorsData = [
     photoUrl: "https://avatars.githubusercontent.com/u/111569738?v=4",
     githubUrl: "https://github.com/sidxhdev",
   },
+    {
+    name: "KAVYA SHAH",
+    photoUrl: "https://avatars.githubusercontent.com/u/220664183?s=400&u=0476ba5880665dd1f8dbb0779d119b12c3c4f19e&v=4",
+    githubUrl: "https://github.com/KAVYASHAH180",
+  },
   
  
   // Add more contributors here
