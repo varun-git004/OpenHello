f36938c5ad309dccd9e0d881830fa492efa67ba5
Aadil-14 <aadilshaikh000000000004@gmail.com>
2025-09-17 13:09:39 +0530

    Update contributorsData.jsx

--- client/src/data/contributorsData.jsx
+++ client/src/data/contributorsData.jsx
@@ -4,6 +4,10 @@ export const contributorsData = [
     photoUrl: "https://avatars.githubusercontent.com/u/111569738?v=4",
     githubUrl: "https://github.com/sidxhdev",
   },
+  {
+    name:"aadil",
+    photoUrl:"https://avatars.githubusercontent.com/u/220671974?v=4",
+    githubUrl:"https://github.com/Aadil-14",
   
  
   // Add more contributors here
