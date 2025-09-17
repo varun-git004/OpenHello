2fef843128bbcf2b4d2e3b325a86a83ea3ab36b9
ShubhangiRajnale <shubhangiraj956@gmail.com>
2025-09-17 13:09:37 +0530

    Update contributorsData.jsx

--- client/src/data/contributorsData.jsx
+++ client/src/data/contributorsData.jsx
@@ -4,7 +4,11 @@ export const contributorsData = [
     photoUrl: "https://avatars.githubusercontent.com/u/111569738?v=4",
     githubUrl: "https://github.com/sidxhdev",
   },
-  
+   {
+    name: "Shubhangi Rajanale",
+    photoUrl: "https://avatars.githubusercontent.com/u/220670759?v=4",
+    githubUrl: "https://github.com/ShubhangiRajnale",
+  },
  
   // Add more contributors here
 ];
