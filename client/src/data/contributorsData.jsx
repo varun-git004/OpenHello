2515fe3a2919748b07520c7d42606d9e18edfb86
Surya Uniyal <uniyalsurya0@gmail.com>
2025-09-17 13:05:04 +0530

    Update contributorsData.jsx

--- client/src/data/contributorsData.jsx
+++ client/src/data/contributorsData.jsx
@@ -1,8 +1,8 @@
 export const contributorsData = [
   {
-    name: "Siddesh Navthale",
-    photoUrl: "https://avatars.githubusercontent.com/u/111569738?v=4",
-    githubUrl: "https://github.com/sidxhdev",
+    name: "Surya Uniyal",
+    photoUrl: "https://avatars.githubusercontent.com/u/186946137?v=4",
+    githubUrl: "https://github.com/uniyalsurya",
   },
   
  
