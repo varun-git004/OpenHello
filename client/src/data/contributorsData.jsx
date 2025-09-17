ba40be594381c483d60b676b089ff48d80a2b66e
ritupotham <ritupotham-cmpn@atharvacoe.ac.in>
2025-09-17 13:11:09 +0530

    Update contributorsData.jsx

--- client/src/data/contributorsData.jsx
+++ client/src/data/contributorsData.jsx
@@ -4,6 +4,11 @@ export const contributorsData = [
     photoUrl: "https://avatars.githubusercontent.com/u/111569738?v=4",
     githubUrl: "https://github.com/sidxhdev",
   },
+ {
+    name: "Siddesh Navthale",
+    photoUrl: "https://avatars.githubusercontent.com/u/111569738?v=4",
+    githubUrl: "https://github.com/ritupotham",
+  }, 
   
  
   // Add more contributors here
