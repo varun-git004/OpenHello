391d06f2fa8b1a7d79a75f8021697d5bbb5de08b
sahilrewale-09 <rewalesahil9@gmail.com>
2025-09-17 13:24:17 +0530

    Update contributorsData.jsx

--- client/src/data/contributorsData.jsx
+++ client/src/data/contributorsData.jsx
@@ -9,6 +9,11 @@ export const contributorsData = [
     photoUrl: "https://avatars.githubusercontent.com/u/180926010?v=4",
     githubUrl: "https://github.com/manali-0715",
   },
+   {
+    name: "Sahil",
+    photoUrl: "https://avatars.githubusercontent.com/u/223218760?v=4",
+    githubUrl: "https://github.com/sahilrewale-09",
+  },
   
  
   // Add more contributors here
