5e5572255f44aa311fc37fda1ca83f291b9ac421
CodePicsel <144619400+CodePicsel@users.noreply.github.com>
2025-09-17 13:09:37 +0530

    Update contributorsData.jsx

--- client/src/data/contributorsData.jsx
+++ client/src/data/contributorsData.jsx
@@ -4,7 +4,10 @@ export const contributorsData = [
     photoUrl: "https://avatars.githubusercontent.com/u/111569738?v=4",
     githubUrl: "https://github.com/sidxhdev",
   },
-  
- 
+  {
+    name: "Shashank Bankar",
+    photoUrl: "https://avatars.githubusercontent.com/u/144619400?v=4",
+    githubUrl: "https://github.com/CodePicsel",
+  },
   // Add more contributors here
 ];
