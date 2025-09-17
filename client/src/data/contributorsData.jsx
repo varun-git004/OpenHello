9b7525451cb1d4a718ea24b8558ae3e24f99b03f
varun-git004 <varunkamble777@gmail.com>
2025-09-17 12:59:14 +0530

    Update contributorsData.jsx

--- client/src/data/contributorsData.jsx
+++ client/src/data/contributorsData.jsx
@@ -4,7 +4,11 @@ export const contributorsData = [
     photoUrl: "https://avatars.githubusercontent.com/u/111569738?v=4",
     githubUrl: "https://github.com/sidxhdev",
   },
-  
+  {
+    name: "sexy varun",
+    photoUrl: "https://avatars.githubusercontent.com/u/222207883?v=4",
+    githubUrl: "https://github.com/varun-git004",
+  }
  
   // Add more contributors here
 ];
