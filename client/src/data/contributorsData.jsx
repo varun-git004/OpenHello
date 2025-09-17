052d70e7cb7e4c8e7f59a18d4325d2ef882b3bf7
RUDRA <rudraprasaddabre@gmail.com>
2025-09-17 13:01:26 +0530

    Update contributorsData.jsx

--- client/src/data/contributorsData.jsx
+++ client/src/data/contributorsData.jsx
@@ -4,7 +4,11 @@ export const contributorsData = [
     photoUrl: "https://avatars.githubusercontent.com/u/111569738?v=4",
     githubUrl: "https://github.com/sidxhdev",
   },
-  
+  {
+    name: "Rudraprasad Dabre",
+    photoUrl: "https://avatars.githubusercontent.com/u/232739373?s=400&u=e228f465cab74dce5c2b36f3a8b11a459f70ebc5&v=4",
+    githubUrl: "https://github.com/rudra-engg",
+  },
  
   // Add more contributors here
 ];
