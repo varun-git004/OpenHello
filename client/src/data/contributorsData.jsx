c52718145a40b542c69a797dde89d8d1890ef0ba
varun-git004 <varunkamble777@gmail.com>
2025-09-17 13:11:10 +0530

    Update contributorsData.jsx

--- client/src/data/contributorsData.jsx
+++ client/src/data/contributorsData.jsx
@@ -3,8 +3,12 @@ export const contributorsData = [
     name: "Siddesh Navthale",
     photoUrl: "https://avatars.githubusercontent.com/u/111569738?v=4",
     githubUrl: "https://github.com/sidxhdev",
+  }
+  {
+    name: "versetile varun",
+    photoUrl: "https://avatars.githubusercontent.com/u/222207883?v=4",
+    githubUrl: "https://github.com/varun-git004",
   },
-  
  
   // Add more contributors here
 ];
