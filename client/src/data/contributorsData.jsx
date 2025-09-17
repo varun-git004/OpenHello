c81c329f2d4dc69a7e9998efbc38b2358970e8dc
vinipatel2318-cmd <vinipatel2318@gmail.com>
2025-09-17 13:15:01 +0530

    Update contributorsData.jsx

--- client/src/data/contributorsData.jsx
+++ client/src/data/contributorsData.jsx
@@ -4,6 +4,11 @@ export const contributorsData = [
     photoUrl: "https://avatars.githubusercontent.com/u/111569738?v=4",
     githubUrl: "https://github.com/sidxhdev",
   },
+  {
+    name: "vaidehi Patel",
+    photoUrl: "https://avatars.githubusercontent.com/u/232736126?s=400&v=4",
+    githubUrl: "https://github.com/vinipatel2318-cmd",
+  },
   
  
   // Add more contributors here
