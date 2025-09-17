81c8cc729ad40bf8dc3155032d7fa7b9cc87b041
Siddesh Navthale <111569738+sidxhdev@users.noreply.github.com>
2025-09-17 09:09:27 +0530

    Revert "Add Hello Card for taneesh"

--- client/src/data/contributorsData.jsx
+++ client/src/data/contributorsData.jsx
@@ -4,11 +4,6 @@ export const contributorsData = [
     photoUrl: "https://avatars.githubusercontent.com/u/111569738?v=4",
     githubUrl: "https://github.com/sidxhdev",
   },
-  {
-    name: "Taneesh Sawant",
-    photoUrl: "https://avatars.githubusercontent.com/u/179444959?v=4",
-    githubUrl: "https://github.com/TaneeshCodes",
-  },
   
  
   // Add more contributors here
