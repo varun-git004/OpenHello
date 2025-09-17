06e75026a74f1b1823b405a078a3f74568ae84e6
Arya <arpat2006@gmail.com>
2025-09-17 13:09:53 +0530

    Update contributorsData.jsx

--- client/src/data/contributorsData.jsx
+++ client/src/data/contributorsData.jsx
@@ -4,6 +4,11 @@ export const contributorsData = [
     photoUrl: "https://avatars.githubusercontent.com/u/111569738?v=4",
     githubUrl: "https://github.com/sidxhdev",
   },
+  {
+    name: "Arya",
+    photoUrl: "https://avatars.githubusercontent.com/u/220624744?v=4",
+    githubUrl: "https://github.com/Arya9415",
+  },
   
  
   // Add more contributors here
