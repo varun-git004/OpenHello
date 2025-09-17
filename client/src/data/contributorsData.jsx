2544853f8d67e9294fc0d981e97fd861956c0bb1
Yash Satpute <satputeyash121@gmail.com>
2025-09-17 13:17:31 +0530

    Update contributorsData.jsx

--- client/src/data/contributorsData.jsx
+++ client/src/data/contributorsData.jsx
@@ -4,6 +4,11 @@ export const contributorsData = [
     photoUrl: "https://avatars.githubusercontent.com/u/111569738?v=4",
     githubUrl: "https://github.com/sidxhdev",
   },
+    {
+    name: "Yash Satptue",
+    photoUrl: "https://avatars.githubusercontent.com/u/222073891?v=4",
+    githubUrl: "https://github.com/Yash-Satpute",
+  },
   
  
   // Add more contributors here
