e89e22fb65a840862c7330ba87832d4a16b78fdb
ParasPalkar <paraspalkar-inft@atharvacoe.ac.in>
2025-09-17 13:13:09 +0530

    Update contributorsData.jsx

--- client/src/data/contributorsData.jsx
+++ client/src/data/contributorsData.jsx
@@ -4,6 +4,11 @@ export const contributorsData = [
     photoUrl: "https://avatars.githubusercontent.com/u/111569738?v=4",
     githubUrl: "https://github.com/sidxhdev",
   },
+  {
+    name: "Siddesh Navthale",
+    photoUrl: "https://avatars.githubusercontent.com/u/178910823?s=400&v=4",
+    githubUrl: "https://github.com/PaRaSpAlKaR",
+  },
   
  
   // Add more contributors here
