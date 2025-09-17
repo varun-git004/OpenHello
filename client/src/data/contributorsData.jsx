ecaa15e4b3a9fcbe982a20dd66e91fad968a26de
Siddesh Navthale <111569738+sidxhdev@users.noreply.github.com>
2025-09-17 14:23:59 +0530

    Revert "Update contributorsData.jsx"

--- client/src/data/contributorsData.jsx
+++ client/src/data/contributorsData.jsx
@@ -1,13 +1,8 @@
 export const contributorsData = [
   {
-    name: "Varun Vishwakarma",
-    photoUrl: "https://avatars.githubusercontent.com/u/232746416?s=400&u=944122dfadab24b1386992b576593c3c6ca24ead&v=4",
-    githubUrl: "https://github.com/varunvishwakarma-cmpn-sketch",
-  },
-  {
-    name: "Varun Vishwakarma",
-    photoUrl: "https://avatars.githubusercontent.com/u/232746416?s=400&u=944122dfadab24b1386992b576593c3c6ca24ead&v=4",
-    githubUrl: "https://github.com/varunvishwakarma-cmpn-sketch",
+    name: "Siddesh Navthale",
+    photoUrl: "https://avatars.githubusercontent.com/u/111569738?v=4",
+    githubUrl: "https://github.com/sidxhdev",
   },
 
     {
