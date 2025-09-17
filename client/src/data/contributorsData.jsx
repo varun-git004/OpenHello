445f5459ab09dfb22fe82317d1f873320edd4cfb
gaurangmhatre0210 <gaurangmhatre-cmpn@atharvacoe.ac.in>
2025-09-17 13:15:52 +0530

    Update contributorsData.jsx

--- client/src/data/contributorsData.jsx
+++ client/src/data/contributorsData.jsx
@@ -4,6 +4,11 @@ export const contributorsData = [
     photoUrl: "https://avatars.githubusercontent.com/u/111569738?v=4",
     githubUrl: "https://github.com/sidxhdev",
   },
+   {
+    name: "gaurang mhatre",
+    photoUrl: "https://avatars.githubusercontent.com/u/232735711?v=4",
+    githubUrl: "https://github.com/gaurangmhatre0210",
+  },
   
  
   // Add more contributors here
