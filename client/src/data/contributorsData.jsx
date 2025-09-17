913528ec10cb62f371c14efa57d9b22176369b95
Sumit Rathod <itzsumitr995@gmail.com>
2025-09-17 13:09:39 +0530

    New Version by Sumit Rathod

--- client/src/data/contributorsData.jsx
+++ client/src/data/contributorsData.jsx
@@ -10,4 +10,5 @@ export const contributorsData = [
     name: "Sumit Rathod",
     photoUrl: "https://avatars.githubusercontent.com/u/182794567?s=400&u=f7721b950bf735af04cb93011b238987bb9f99b2&v=4",
     githubUrl: "https://github.com/Sumitr995",
+  },
 ];
